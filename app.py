fc842b6a4ba09a2ef1731a0d8e5600bc3708cb44
Meghashyama Sarma <shyama.gowra@gmail.com>
2022-11-08 19:26:18 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from numerize.numerize import numerize
 
 st.set_page_config(page_title = 'Facebook Ad Campaign Dashboard',
                     layout='wide',
-                    initial_sidebar_state='expanded')
+                    initial_sidebar_state='collapsed')
 
 @st.cache
 def get_data():
